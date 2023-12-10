a6777942df81b4dc8d9738fc8952b9d1ad5ee949
MUNING CHEN <muning.chen@ufl.edu>
2023-12-10 16:58:00 -0500

    finished the challenge

--- book_search.js
+++ book_search.js
@@ -21,13 +21,37 @@
  function findSearchTermInBooks(searchTerm, scannedTextObj) {
     /** You will need to implement your search and 
      * return the appropriate object here. */
+    const results = [];
 
-    var result = {
-        "SearchTerm": "",
-        "Results": []
+    for (const book of scannedTextObj) {
+        for (const content of book.Content) {
+            const text = content.Text;
+            const regex = new RegExp(`\\b${searchTerm}\\b`, 'g'); 
+
+            let match;
+            while ((match = regex.exec(text)) !== null) {
+                // Check for duplicate entry before pushing to results
+                const isDuplicate = results.some(result =>
+                    result.ISBN === book.ISBN &&
+                    result.Page === content.Page &&
+                    result.Line === content.Line
+                );
+
+                if (!isDuplicate) {
+                    results.push({
+                        ISBN: book.ISBN,
+                        Page: content.Page,
+                        Line: content.Line
+                    });
+                }
+            }
+        }
+    }
+
+    return {
+        SearchTerm: searchTerm,
+        Results: results
     };
-    
-    return result; 
 }
 
 /** Example input object. */
@@ -101,4 +125,217 @@ if (test2result.Results.length == 1) {
     console.log("FAIL: Test 2");
     console.log("Expected:", twentyLeaguesOut.Results.length);
     console.log("Received:", test2result.Results.length);
+}
+
+/** BOOKS FOR TEST */
+/** There are two books in this array object */
+const Books = [
+    {
+        "Title": "The Road not Taken",
+        "ISBN": "1234567890",
+        "Content": [
+            {
+                "Page": 1,
+                "Line": 1,
+                "Text": "Two roads diverged in a yellow wood,"
+            },
+            {
+                "Page": 1,
+                "Line": 2,
+                "Text": "And sorry I could not travel both"
+            },
+            {
+                "Page": 1,
+                "Line": 3,
+                "Text": "And be one traveler, long I stood"
+            },
+            {
+                "Page": 1,
+                "Line": 4,
+                "Text": "And looked down one as far as I could"
+            },
+            {
+                "Page": 1,
+                "Line": 5,
+                "Text": "To where it bent in the undergrowth;"
+            },
+            {
+                "Page": 2,
+                "Line": 1,
+                "Text": "Then took the other, as just as fair,"
+            },
+            {
+                "Page": 2,
+                "Line": 2,
+                "Text": "And having perhaps the better claim,"
+            },
+            {
+                "Page": 2,
+                "Line": 3,
+                "Text": "Because it was grassy and wanted wear;"
+            },
+            {
+                "Page": 2,
+                "Line": 4,
+                "Text": "Though as for that the passing there"
+            },
+            {
+                "Page": 2,
+                "Line": 5,
+                "Text": "Had worn them really about the same,"
+            },
+            
+        ]
+    },
+    {
+        "title": "Do not go gentle into that good night",
+        "ISBN": "0987654321",
+        "Content":
+        [
+            {
+                "Page": 10,
+                "Line": 6,
+                "Text": "Do not go gentle into that good night,"
+            },
+            {
+                "Page": 10,
+                "Line": 7,
+                "Text": "Old age should burn and rave at close of day;"
+            },
+            {
+                "Page": 10,
+                "Line": 8,
+                "Text": "Rage, rage against the dying of the light."
+            },
+            {
+                "Page": 10,
+                "Line": 20,
+                "Text": "And learn, too late, they grieved it on its way,"
+            }
+
+        ]
+    }
+];
+
+
+/** Positive Test */
+/** This Test Searched for the term And, which appeared in both books, on different pages, and in different lines */
+
+const positiveTestResult = findSearchTermInBooks("And", Books);
+const positiveTestExpected = {
+    SearchTerm: "And",
+    Results: [
+        {
+            ISBN: "1234567890",
+            Page: 1,
+            Line: 2
+        },
+        {
+            ISBN: "1234567890",
+            Page: 1,
+            Line: 3
+        },
+        {
+            ISBN: "1234567890",
+            Page: 1,
+            Line: 4
+        },
+        {
+            ISBN: "1234567890",
+            Page: 2,
+            Line: 2
+        },
+        {
+            ISBN: "0987654321",
+            Page: 10,
+            Line: 20
+        }
+
+        
+    ]
+};
+
+if (JSON.stringify(positiveTestResult) === JSON.stringify(positiveTestExpected)) {
+    console.log("PASS: Positive Test");
+} else {
+    console.log("FAIL: Positive Test");
+    console.log("Expected:", positiveTestExpected);
+    console.log("Received:", positiveTestResult);
+}
+
+/** negative test */
+/** It searches for the term missing, which does not appear in the text */
+const negativeTestResult = findSearchTermInBooks("missing", Books);
+const negativeTestExpected = {
+    SearchTerm: "missing",
+    Results: []
+};
+
+if (JSON.stringify(negativeTestResult) === JSON.stringify(negativeTestExpected)) {
+    console.log("PASS: Negative Test");
+} else {
+    console.log("FAIL: Negative Test");
+    console.log("Expected:", NegativeTestExpected);
+    console.log("Received:", NegativeTestResult);
+}
+
+/** case-sensitive test */
+/** The positive test already accounts for case-sensitivity by searching for "And" */
+/** Here we search for "and", which should return different results from positive test above */
+const caseSensitiveTestResult = findSearchTermInBooks("and", Books);
+const caseSensitiveTestExpected = {
+    SearchTerm: "and",
+    Results: [
+        {
+            ISBN: "1234567890",
+            Page: 2,
+            Line: 3
+        },
+        {
+            ISBN: "0987654321",
+            Page: 10,
+            Line: 7
+        }
+    ]
+};
+
+if (JSON.stringify(caseSensitiveTestResult) === JSON.stringify(caseSensitiveTestExpected)) {
+    console.log("PASS: Case Sensitive Test");
+} else {
+    console.log("FAIL: Case Sensitive Test");
+    console.log("Expected:", caseSensitiveTestExpected);
+    console.log("Received:", caseSensitiveTestResult);
+}
+
+/** duplicate test */
+/** when the search term appear in one line twice*/
+/** the line should only be reported once */
+const duplicateTestResult = findSearchTermInBooks("as", Books);
+const duplicateTestExpected = {
+    SearchTerm: "as",
+    Results: [
+        {
+            ISBN: "1234567890",
+            Page: 1,
+            Line: 4
+        },
+        {
+            ISBN: "1234567890",
+            Page: 2,
+            Line: 1
+        },
+        {
+            ISBN: "1234567890",
+            Page: 2,
+            Line: 4
+        }
+    ]
+}
+
+if (JSON.stringify(duplicateTestResult) === JSON.stringify(duplicateTestExpected)) {
+    console.log("PASS: Duplicate Test");
+} else {
+    console.log("FAIL: Duplicate Test");
+    console.log("Expected:", DuplicateTestExpected);
+    console.log("Received:", DuplicateTestResult);
 }
